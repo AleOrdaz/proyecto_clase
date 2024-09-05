bf7fc3721c7354c793aadc6052e2357824c698fa
Alejandro Ordaz <oordaz@nrtec.com.mx>
2024-09-05 08:51:43 -0600

    lista

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:primera_app_8_9/utils/constants.dart' as con;
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -14,377 +15,34 @@ class _HomeState extends State<Home> {
       body: Stack(
         children: [
           Padding(
-              padding: EdgeInsets.all(50.0),
+              padding: EdgeInsets.all(20.0),
               ///Crea un scrool
               child: SingleChildScrollView(
                 child:Column(
                   children: [
-                    createCard(),
-                    SizedBox(height: 15.0,),
                     Container(
-                        padding: EdgeInsets.all(15.0),
-                        color: Colors.grey,
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                  flex: 6,
-                                  child: Container(height: 50, width: 50, color: Colors.red,),
-                                ),
-                                Expanded(
-                                    flex: 6,
-                                    child: Column(
-                                      ///Toma el tamaño de los widgets hijos
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Text('Hola'),
-                                        Text('mundo')
-                                      ],
-                                    )
-                                )
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Expanded(flex: 2, child: Text('#'),),
-                                Expanded(flex: 5, child: Icon(Icons.edit),),
-                                Expanded(flex: 5,child: Icon(Icons.delete),),
-                              ],
-                            ),
-                          ],
-                        )
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(15.0),
-                      color: Colors.grey,
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 8,
-                            child: Text('Row1'),
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: Icon(Icons.edit),
-                          ),
-                          Expanded(
-                            flex:2,
-                            child: Icon(Icons.delete),
-                          ),
-                        ],
+                      height: 800,
+                      width: 400,
+                      child: ListView.builder(
+                          padding: const EdgeInsets.all(8),
+                          itemCount: con.lista.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            ///tipo var es una variable que se adapta o toma el
+                            ///tipo de dato que se le asigna
+                            var datos = con.lista[index].toString().split('#'); ///$ - no usar
+                            print('ID: ${datos[0]}');
+
+                            return createCard(
+                              txtNegritas: datos[1],
+                              txtNormal1: datos[2],
+                              txtNormal2: datos[3],
+                              numero: datos[4],
+                            );
+                          }
+                        //const SizedBox(height: 35.0,),
                       ),
-                    ),
-                    SizedBox(height: 15.0,),
-                    Container(
-                        padding: EdgeInsets.all(15.0),
-                        color: Colors.grey,
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                  flex: 6,
-                                  child: Container(height: 50, width: 50, color: Colors.red,),
-                                ),
-                                Expanded(
-                                    flex: 6,
-                                    child: Column(
-                                      ///Toma el tamaño de los widgets hijos
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Text('Hola'),
-                                        Text('mundo')
-                                      ],
-                                    )
-                                )
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Expanded(flex: 2, child: Text('#'),),
-                                Expanded(flex: 5, child: Icon(Icons.edit),),
-                                Expanded(flex: 5,child: Icon(Icons.delete),),
-                              ],
-                            ),
-                          ],
-                        )
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(15.0),
-                      color: Colors.grey,
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 8,
-                            child: Text('Row1'),
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: Icon(Icons.edit),
-                          ),
-                          Expanded(
-                            flex:2,
-                            child: Icon(Icons.delete),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 15.0,),
-                    Container(
-                        padding: EdgeInsets.all(15.0),
-                        color: Colors.grey,
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                  flex: 6,
-                                  child: Container(height: 50, width: 50, color: Colors.red,),
-                                ),
-                                Expanded(
-                                    flex: 6,
-                                    child: Column(
-                                      ///Toma el tamaño de los widgets hijos
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Text('Hola'),
-                                        Text('mundo')
-                                      ],
-                                    )
-                                )
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Expanded(flex: 2, child: Text('#'),),
-                                Expanded(flex: 5, child: Icon(Icons.edit),),
-                                Expanded(flex: 5,child: Icon(Icons.delete),),
-                              ],
-                            ),
-                          ],
-                        )
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(15.0),
-                      color: Colors.grey,
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 8,
-                            child: Text('Row1'),
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: Icon(Icons.edit),
-                          ),
-                          Expanded(
-                            flex:2,
-                            child: Icon(Icons.delete),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 15.0,),
-                    Container(
-                        padding: EdgeInsets.all(15.0),
-                        color: Colors.grey,
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                  flex: 6,
-                                  child: Container(height: 50, width: 50, color: Colors.red,),
-                                ),
-                                Expanded(
-                                    flex: 6,
-                                    child: Column(
-                                      ///Toma el tamaño de los widgets hijos
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Text('Hola'),
-                                        Text('mundo')
-                                      ],
-                                    )
-                                )
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Expanded(flex: 2, child: Text('#'),),
-                                Expanded(flex: 5, child: Icon(Icons.edit),),
-                                Expanded(flex: 5,child: Icon(Icons.delete),),
-                              ],
-                            ),
-                          ],
-                        )
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(15.0),
-                      color: Colors.grey,
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 8,
-                            child: Text('Row1'),
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: Icon(Icons.edit),
-                          ),
-                          Expanded(
-                            flex:2,
-                            child: Icon(Icons.delete),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 15.0,),
-                    Container(
-                        padding: EdgeInsets.all(15.0),
-                        color: Colors.grey,
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                  flex: 6,
-                                  child: Container(height: 50, width: 50, color: Colors.red,),
-                                ),
-                                Expanded(
-                                    flex: 6,
-                                    child: Column(
-                                      ///Toma el tamaño de los widgets hijos
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Text('Hola'),
-                                        Text('mundo')
-                                      ],
-                                    )
-                                )
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Expanded(flex: 2, child: Text('#'),),
-                                Expanded(flex: 5, child: Icon(Icons.edit),),
-                                Expanded(flex: 5,child: Icon(Icons.delete),),
-                              ],
-                            ),
-                          ],
-                        )
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(15.0),
-                      color: Colors.grey,
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 8,
-                            child: Text('Row1'),
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: Icon(Icons.edit),
-                          ),
-                          Expanded(
-                            flex:2,
-                            child: Icon(Icons.delete),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 15.0,),
-                    Container(
-                        padding: EdgeInsets.all(15.0),
-                        color: Colors.grey,
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                  flex: 6,
-                                  child: Container(height: 50, width: 50, color: Colors.red,),
-                                ),
-                                Expanded(
-                                    flex: 6,
-                                    child: Column(
-                                      ///Toma el tamaño de los widgets hijos
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Text('Hola'),
-                                        Text('mundo')
-                                      ],
-                                    )
-                                )
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Expanded(flex: 2, child: Text('#'),),
-                                Expanded(flex: 5, child: Icon(Icons.edit),),
-                                Expanded(flex: 5,child: Icon(Icons.delete),),
-                              ],
-                            ),
-                          ],
-                        )
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(15.0),
-                      color: Colors.grey,
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 8,
-                            child: Text('Row1'),
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: Icon(Icons.edit),
-                          ),
-                          Expanded(
-                            flex:2,
-                            child: Icon(Icons.delete),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 15.0,),
-                    Container(
-                        padding: EdgeInsets.all(15.0),
-                        color: Colors.grey,
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                  flex: 6,
-                                  child: Container(height: 50, width: 50, color: Colors.red,),
-                                ),
-                                Expanded(
-                                    flex: 6,
-                                    child: Column(
-                                      ///Toma el tamaño de los widgets hijos
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Text('Hola'),
-                                        Text('mundo')
-                                      ],
-                                    )
-                                )
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Expanded(flex: 2, child: Text('#'),),
-                                Expanded(flex: 5, child: Icon(Icons.edit),),
-                                Expanded(flex: 5,child: Icon(Icons.delete),),
-                              ],
-                            ),
-                          ],
-                        )
-                    ),
+                    )
+
                   ],
                 ),
               )
@@ -396,15 +54,25 @@ class _HomeState extends State<Home> {
 }
 
 class createCard extends StatelessWidget {
+  ///Variable con final es decir que se le asignará valor más adelante
+  final String txtNegritas;
+  final String txtNormal1;
+  final String txtNormal2;
+  final String numero;
   const createCard({
-    super.key,
+    super.key, required this.txtNegritas, required this.txtNormal1,
+    required this.txtNormal2, required this.numero,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(15.0),
-      color: Colors.grey,
+      ///color: Colors.grey, /// -------------------------------------->  Estos dos atributos
+      decoration: BoxDecoration(      ///                                juntos, marcará un error por
+        borderRadius: BorderRadius.circular(40),    ///                  incompatibilidad de atributos
+        color: Colors.black54, /// ------------------------------------>
+      ),
       child: Row(
         children: [
           Expanded(
---
+++ lib/utils/constants.dart
@@ -0,0 +1,13 @@
+const List lista = [
+  //ID # 1er texto # ... # num
+  '1#Texto 1#Texto 2#Texto 3#1',
+  '2#Texto 21#Texto 22#Texto 23#21',
+  '3#Texto 31#Texto 32#Texto 33#31',
+  '4#Texto 41#Texto 42#Texto 43#41',
+  '5#Texto 51#Texto 52#Texto 53#51',
+  '6#Texto 61#Texto 62#Texto 63#61',
+  '7#Texto 71#Texto 72#Texto 73#71',
+  '8#Texto 81#Texto 82#Texto 83#81',
+  '9#Texto 91#Texto 92#Texto 93#91',
+  '10#Texto 101#Texto 102#Texto 103#101',
+];
