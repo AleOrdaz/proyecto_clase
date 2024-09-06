3bb95905c8a3aaa93123c70c16b51d7259195f48
Alejandro Ordaz <oordaz@nrtec.com.mx>
2024-09-06 09:45:21 -0600

    Tarea

--- lib/home.dart
+++ lib/home.dart
@@ -11,6 +11,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    ///Forma de obtener el tamaño de nuestra pantalla
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
         children: [
@@ -20,9 +22,24 @@ class _HomeState extends State<Home> {
               child: SingleChildScrollView(
                 child:Column(
                   children: [
+                    InkWell(
+                      onTap: () {
+                        ///Actualiza el estado de la vista
+                        setState(() {
+                          bandCambiarDeEstado = bandCambiarDeEstado ? false : true;
+                        });
+                      },
+                      child: bandCambiarDeEstado ? Container(
+                        width: 100,
+                        height: bandCambiarDeEstado ? 100 : 50,
+                        color: bandCambiarDeEstado ? Colors.red : Colors.green,
+                      ) : Text('Hola'),
+                    ),
                     Container(
-                      height: 800,
-                      width: 400,
+                      ///MediaQuery.of(context).size.height * 0.5 = /2 -> 50%
+                      ///MediaQuery.of(context).size.height * 0.25 = /4 -> 25%
+                      height: size.height * .5, ///100%
+                      width: size.width, ///100%
                       child: ListView.builder(
                           padding: const EdgeInsets.all(8),
                           itemCount: con.lista.length,
@@ -37,6 +54,7 @@ class _HomeState extends State<Home> {
                               txtNormal1: datos[2],
                               txtNormal2: datos[3],
                               numero: datos[4],
+                              id: datos[0],
                             );
                           }
                         //const SizedBox(height: 35.0,),
@@ -59,9 +77,10 @@ class createCard extends StatelessWidget {
   final String txtNormal1;
   final String txtNormal2;
   final String numero;
+  final String id;
   const createCard({
     super.key, required this.txtNegritas, required this.txtNormal1,
-    required this.txtNormal2, required this.numero,
+    required this.txtNormal2, required this.numero, required this.id,
   });
 
   @override
@@ -77,11 +96,17 @@ class createCard extends StatelessWidget {
         children: [
           Expanded(
             flex: 8,
-            child: Text('Row1'),
+            child: Text(txtNegritas),
           ),
           Expanded(
             flex: 2,
-            child: Icon(Icons.edit),
+            ///Convertir cualquier widget con acción de boton
+            child: InkWell(
+              onTap: () {
+                print("Se edita el dato del id: $id");
+              },
+              child: Icon(Icons.edit)
+            ),
           ),
           Expanded(
             flex:2,
